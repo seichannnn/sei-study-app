e33e15868899eca342477830bf8c4a6941db3937
seichannnn <kulongzhimei70@gmail.com>
2026-07-08 14:02:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // StudyFlow Service Worker - 通知クリックハンドラ & オフライン対応
-const CACHE_NAME = 'studyflow-v10';
+const CACHE_NAME = 'studyflow-v11';
 
 // Install: キャッシュは最小限に
 self.addEventListener('install', (event) => {
